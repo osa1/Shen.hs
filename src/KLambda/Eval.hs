23e335d839036e43ae1e6e54ab0ce5cceaf9a1fe
osa1 <omeragacan@gmail.com>
2013-02-24 14:26:50 +0200

    helpers, some refactoring, eval

--- src/KLambda/Eval.hs
+++ src/KLambda/Eval.hs
@@ -1,133 +1,78 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving, NoMonomorphismRestriction #-}
-module Eval where
-
-import Parser
+{-# OPTIONS_GHC -Wall #-}
+module KLambda.Eval where
 
 import qualified Data.HashMap.Strict as M
 
-import Data.Maybe (fromJust)
-import Control.Monad.State
 import Control.Monad.Error
-import Control.Applicative
+import Control.Monad.State
+import Data.Maybe (fromJust)
 
-data Val
-    = SymV Symbol
-    | BoolV Bool
-    | StringV String
-    | NumberV Int
-    | StreamV
-    | ExceptionV KlException
-    | VectorV
-    | FunctionV
-    | ListV
-    | TupleV
-    | ClosureV
-    deriving Show
-
-type Env = M.HashMap Symbol Val
-
-data KlException = KlException String deriving Show
-
-instance Error KlException where
-    strMsg = KlException
-
-newtype Kl a = Kl { runKl :: StateT Env (ErrorT KlException IO) a }
-    deriving ( Functor, Applicative, Monad, MonadState Env
-             , MonadIO, MonadError KlException )
+import KLambda.Types
+import Prelude hiding (exp)
 
 evalKl :: Env -> Kl a -> IO (Either KlException a)
 evalKl env k = runErrorT $ evalStateT (runKl k) env
 
-eval :: Sexp -> Kl Val
-eval (Sym symbol) = liftM (fromJust . M.lookup symbol) get
-eval (Bool True)  = return $ BoolV True
-eval (Bool False) = return $ BoolV False
-eval (String s)   = return $ StringV s
-eval (Number n)   = return $ NumberV (read n)
+evalWEnv :: Env -> Kl a -> Kl a
+evalWEnv env kl = Kl . lift $ evalStateT (runKl kl) env
 
+ensureType' :: Exp -> Type -> Kl Val
+ensureType' exp ty = do
+    v1 <- eval exp
+    let t = typeOf v1
+    if t == ty
+      then return v1
+      else throwError $ TypeError { foundTy = t, expectedTy = ty }
 
--- Boolean operations --------------------------------------
-eval (App (Sym "if") [guard, thenE, elseE]) = do
-    BoolV guardV <- eval guard
-    if guardV
-      then eval thenE
-      else eval elseE
+intern :: Val -> Kl Val
+intern v = do
+    VStr s <- ensureType v TyStr
+    return $ VSym s
 
-eval (App (Sym "and") [arg1, arg2]) = do
-    BoolV arg1' <- eval arg1
-    if arg1'
-      then eval arg2
-      else return $ BoolV False
+value :: [Exp] -> Kl Val
+value exps = do
+    [e1]   <- ensureArity 1 exps
+    v1     <- eval e1
+    VSym s <- ensureType v1 TySym
+    env    <- get
+    return $ fromJust (M.lookup s env)
 
-eval (App (Sym "or") [arg1, arg2]) = do
-    BoolV arg1' <- eval arg1
-    if arg1'
-      then return $ BoolV True
-      else eval arg2
+set' :: [Exp] -> Kl Val
+set' exps = do
+    [e1, e2] <- ensureArity 2 exps
+    v1       <- eval e1
+    VSym s   <- ensureType v1 TySym
+    v2       <- eval e2
+    modify $ \m -> M.insert s v2 m
+    return v2
 
-eval (App (Sym "cond") cases) = evalCases cases
-  where evalCases [] = return $ SymV "nil"
-        evalCases ((App guard [body]):rest) = do
-          BoolV guardv <- eval guard
-          if guardv
-            then eval body
-            else evalCases rest
+apply :: Func -> [Exp] -> Kl Val
+apply f [] = do
+    return $ VFun f
 
--- Symbols -------------------------------------------------
-eval (App (Sym "intern") [s]) = do
-    StringV s' <- eval s
-    return $ SymV s'
+apply (Closure env arg body) [a] = do
+    av <- eval a
+    evalWEnv (M.insert arg av env) (eval body)
 
--- Strings -------------------------------------------------
-eval (App (Sym "pos") [string, idx]) = do
-    StringV stringv <- eval string
-    NumberV idxv    <- eval idx
-    return $ StringV [stringv !! idxv]
+apply c (a:as) = do
+    f <- apply c [a]
+    case f of
+      VFun c' -> apply c' as
+      _ -> error "can't apply a non function value"
 
-eval (App (Sym "tlstr") [string]) = do
-    StringV stringv <- eval string
-    return $ StringV (tail stringv)
-
-eval (App (Sym "cn") [string1, string2]) = do
-    StringV strv1 <- eval string1
-    StringV strv2 <- eval string2
-    return $ StringV (strv1 ++ strv2)
-
-eval (App (Sym "str") [atom]) = liftM (StringV . show) (eval atom)
-
-eval (App (Sym "n->string") [num]) = liftM (StringV . show) (eval num)
-
-eval (App (Sym "string->n") [nums]) = do
-    StringV s <- eval nums
-    return $ NumberV (read s)
-
--- Assignments ---------------------------------------------
-eval (App (Sym "set") [Sym s, val]) = do
-    val' <- eval val
-    modify (M.insert s val')
-    return val'
-
-eval (App (Sym "value") [Sym s]) = liftM (fromJust . M.lookup s) get
-
--- Error handling ------------------------------------------
-eval (App (Sym "simple-error") [s]) = do
-    StringV str <- eval s
-    throwError $ KlException str
-
-{-eval (App (Sym "trap-error") [fun, handler]) = do
-    r <- eval fun
-    case r of
-      ExceptionV exc -> do
-        handler' <- eval handler
-
-        eval (App handler [ExceptionV exc])
-      _ -> return r-}
-
-eval (App (Sym "error-to-string") [ex]) = do
-    ExceptionV exc <- eval ex
-    return $ StringV (show exc)
-
-
--- Lists ---------------------------------------------------
-
-
+eval :: Exp -> Kl Val
+eval (ESym s)  = return $ VSym s
+eval (EBool b) = return $ VBool b
+eval (EStr s)  = return $ VStr s
+eval (ENum n)  = return $ VNum n
+eval EUnit     = return $ VList []
+eval (ELambda param body) = do
+    env <- get
+    return $ VFun (Closure env param body)
+eval (EApp exp args) = do
+    f <- eval exp
+    case f of
+      VSym "value"  -> value args
+      VSym "set"    -> set'  args
+      VFun l        -> apply l args
+      _ -> error "can't apply a non function value"
--- src/KLambda/Types.hs
+++ src/KLambda/Types.hs
@@ -1,29 +1,60 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, FlexibleInstances #-}
 module KLambda.Types where
 
+import qualified Data.HashMap.Strict as M
+
 import Control.Monad.Error
+import Control.Monad.State
+import Control.Applicative
 
 type Number = Double
 type Symbol = String
 
 data Type
-    = TySymbol | TyString | TyNumber | TyBool | TyStream | TyException
-    | TyVector | TyFunction | TyList | TyTuple | TyClosure | TyContinuation
-    deriving Show
+    = TySym | TyStr | TyNum | TyBool | TyStream | TyExc
+    | TyVec | TyFun | TyList | TyTuple | TyClos | TyCont
+    deriving (Show, Eq)
 
-data KLException
-    = TypeError { found :: Type, expected :: Type }
+data KlException
+    = TypeError     { foundTy :: Type, expectedTy :: Type }
+    | ArityMismatch { foundAr :: Int, expectedAr :: Int }
     | ErrMsg String
 
-instance Error KLException where
+instance Error KlException where
     strMsg = ErrMsg
 
-data Value
+type Env = M.HashMap Symbol Val
+
+data Val
     = VSym String
     | VBool Bool
     | VStr String
     | VNum Number
+    | VList [Val]
+    | VFun Func
     deriving Show
+
+class KlVal a where
+    klVal :: a -> Val
+
+instance KlVal Val    where klVal = id
+instance KlVal Int    where klVal = VNum . fromIntegral
+instance KlVal Bool   where klVal = VBool
+instance KlVal Char   where klVal = VStr . (:[])
+instance KlVal Double where klVal = VNum
+instance KlVal a => KlVal [a]
+    where klVal a = VList $ map klVal a
+instance KlVal ([Exp] -> Kl Val)
+    where klVal = VFun . StdFun
+
+newtype Kl a = Kl { runKl :: StateT Env (ErrorT KlException IO) a }
+    deriving ( Functor, Applicative, Monad, MonadState Env
+             , MonadIO, MonadError KlException )
+
+data Func = Closure Env Symbol Exp
+          | StdFun ([Exp] -> Kl Val)
+
+instance Show Func where show _ = "func"
 
 data Exp
     = ESym  String
@@ -35,3 +66,21 @@ data Exp
     | ELambda Symbol Exp
     | EUnit
     deriving Show
+
+typeOf :: Val -> Type
+typeOf VSym{}  = TySym
+typeOf VBool{} = TyBool
+typeOf VStr{}  = TyStr
+typeOf VNum{}  = TyNum
+typeOf VList{} = TyList
+typeOf VFun{} = TyClos
+
+ensureType :: Val -> Type -> Kl Val
+ensureType val ty
+  | typeOf val == ty = return val
+  | otherwise  = throwError $ TypeError { foundTy = typeOf val, expectedTy = ty }
+
+ensureArity :: Int -> [a] -> Kl [a]
+ensureArity n l
+  | n == length l = return l
+  | otherwise     = throwError $ ArityMismatch { foundAr = length l, expectedAr = n }
