9a9d5dff04a8595a5d27ca7524e1b8b9b9b99e85
osa1 <omeragacan@gmail.com>
2013-02-27 12:05:46 +0200

    EnsureType, KlFun, curried implementation of stdandard functions

--- src/KLambda/Fun.hs
+++ src/KLambda/Fun.hs
@@ -1,5 +1,5 @@
 {-# OPTIONS_GHC -Wall #-}
-{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE Rank2Types, ScopedTypeVariables, FlexibleInstances, FlexibleContexts #-}
 module KLambda.StdFun where
 
 import qualified Data.HashMap.Strict as M
@@ -9,119 +9,122 @@ import Data.Maybe (fromJust)
 
 import KLambda.Types
 import KLambda.Eval
+import KLambda.Env
 
-klEnsureType :: Type -> [Exp] -> Kl Val
-klEnsureType ty exps = do
-    [e1] <- ensureArity 1 exps
-    v1   <- eval e1
-    return . klVal $ if typeOf v1 == ty
-                       then True
-                       else False
+klEnsureType :: Type -> Func
+klEnsureType ty = StdFun $ \e -> do
+  v1 <- eval e
+  return . klVal $ if typeOf v1 == ty
+                     then True
+                     else False
+
+instance KlFun Func where
+    apply (Closure env argName body) arg = do
+      arg' <- eval arg
+      evalWEnv (insertSymEnv argName arg' env) body
+    apply (StdFun f) arg = apply f arg
+
+instance KlFun (Kl Val) where
+    apply v arg = do
+      v' <- v
+      case v' of
+        VFun f -> apply f arg
+        VSym (Symbol s) -> do
+          fenv <- gets fst
+          apply (fromJust $ M.lookup s fenv) arg
+        inv ->
+          throwError $ TypeError { expectedTy = TyFun, foundTy = typeOf inv }
+
+instance KlVal (Exp -> Kl Val) where klVal = VFun . StdFun
 
 -- String operations
 -- --------------------------------------------------------
 
-pos, tlstr, cn, str, strp, nToStr, strToN :: [Exp] -> Kl Val
-pos exps = do
-    [e1, e2] <- ensureArity 2 exps
-    VStr s   <- ensureType' e1 TyStr
-    VNum n   <- ensureType' e2 TyNum
-    return $ VStr [s !! (floor n)]
+pos, tlstr, cn, str, strp, nToStr, strToN :: Func
+pos = StdFun $ \e1 e2 -> do
+  s             <- ensureType =<< eval e1
+  (n :: Double) <- ensureType =<< eval e2
+  return $ VStr [s !! (floor n)]
 
-tlstr exps = do
-    [e1]   <- ensureArity 1 exps
-    VStr s <- ensureType' e1 TyStr
-    return $ VStr (tail s)
+tlstr = StdFun $ \e -> do
+  s <- ensureType =<< eval e
+  return $ VStr (tail s)
 
-cn exps = do
-    [e1, e2] <- ensureArity 2 exps
-    VStr s1  <- ensureType' e1 TyStr
-    VStr s2  <- ensureType' e2 TyStr
-    return $ VStr (s1 ++ s2)
+cn = StdFun $ \e1 e2 -> do
+  s1 <- ensureType =<< eval e1
+  s2 <- ensureType =<< eval e2
+  return $ VStr (s1 ++ s2)
 
-str exps = do
-    [e1] <- ensureArity 1 exps
-    v    <- eval e1
-    return $ VStr (show v)
+str = StdFun $ \e -> do
+  v <- eval e
+  return $ VStr (show v)
 
 strp = klEnsureType TyStr
 
 nToStr = str
 
-strToN exps = do
-    [e1]   <- ensureArity 1 exps
-    VStr n <- ensureType' e1 TyStr
-    return $ VNum (read n)
+strToN = StdFun $ \e -> do
+  n <- ensureType =<< eval e
+  return $ VNum (read n)
 
 -- Lists
 -- --------------------------------------------------------
 
-cons, hd, tl, consp :: [Exp] -> Kl Val
-cons exps = do
-    [e1, e2] <- ensureArity 2 exps
-    v1       <- eval e1
-    VList l  <- ensureType' e2 TyList
-    return $ klVal (v1:l)
+cons, hd, tl, consp :: Func
+cons = StdFun $ \e1 e2 -> do
+  vl <- eval e1
+  l  <- ensureType =<< eval e2
+  return $ klVal (vl:l)
 
-hd exps = do
-    [e1]    <- ensureArity 1 exps
-    VList l <- ensureType' e1 TyList
-    return $ head l
+hd = StdFun $ \e -> do
+  (l :: [Val]) <- ensureType =<< eval e
+  return $ head l
 
-tl exps = do
-    [e1]    <- ensureArity 1 exps
-    VList l <- ensureType' e1 TyList
-    return $ klVal (tail l)
+tl = StdFun $ \e -> do
+  (l :: [Val]) <- ensureType =<< eval e
+  return $ klVal (tail l)
 
 consp = klEnsureType TyList
 
 -- Arithmetic
 -- --------------------------------------------------------
 
-mkArithFun :: forall a. KlVal a => (Number -> Number -> a) -> [Exp] -> Kl Val
-mkArithFun op exps = do
-    [e1, e2] <- ensureArity 2 exps
-    VNum n1  <- ensureType' e1 TyNum
-    VNum n2  <- ensureType' e2 TyNum
-    return $ klVal (n1 `op` n2)
+mkArithFun :: forall a. KlVal a => (Double -> Double -> a) -> Func
+mkArithFun op = StdFun $ \e1 e2 -> do
+  n1 <- ensureType =<< eval e1
+  n2 <- ensureType =<< eval e2
+  return $ klVal (n1 `op` n2)
 
-numberp :: [Exp] -> Kl Val
+numberp :: Func
 numberp = klEnsureType TyNum
 
 -- Assignments
 -- --------------------------------------------------------
 
-set' :: [Exp] -> Kl Val
-set' exps = do
-    [e1, e2] <- ensureArity 2 exps
-    case e1 of
-      ESym s -> do
-        v2 <- eval e2
-        modify $ \env -> insertSymEnv s v2 env
-        return v2
-      _ -> throwError $ ErrMsg "set to a non-symbol expression"
+set', value :: Func
+set' = StdFun $ \e1 e2 -> do
+  s <- ensureType =<< eval e1
+  v <- eval e2
+  modify $ \env -> insertSymEnv s v env
+  return v
 
-value :: [Exp] -> Kl Val
-value exps = do
-    [e1]   <- ensureArity 1 exps
-    VSym s <- ensureType' e1 TySym
-    senv   <- gets symEnv
-    return $ fromJust (M.lookup s senv)
+value = StdFun $ \e -> do
+  s      <- ensureType =<< eval e
+  senv   <- gets symEnv
+  return $ fromJust (M.lookup s senv)
 
 -- Symbols
 -- --------------------------------------------------------
 
-intern :: [Exp] -> Kl Val
-intern exps = do
-    [e1]   <- ensureArity 1 exps
-    VStr s <- ensureType' e1 TyStr
-    return $ VSym s
-
+intern :: Func
+intern = StdFun $ \e -> do
+  s <- ensureType =<< eval e
+  return $ VSym s
 
 -- Standard environment
 -- --------------------------------------------------------
 
-stdEnv :: M.HashMap [Char] ([Exp] -> Kl Val)
+stdEnv :: M.HashMap String Func
 stdEnv = M.fromList
   [ ("pos", pos)
   , ("tlstr", tlstr)
--- src/KLambda/Types.hs
+++ src/KLambda/Types.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving, FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, FlexibleInstances, MultiParamTypeClasses, ExistentialQuantification #-}
+{-# OPTIONS_GHC -Wall #-}
 module KLambda.Types where
 
 import qualified Data.HashMap.Strict as M
@@ -37,6 +38,44 @@ data Val
     | VFun Func
     deriving Show
 
+class EnsureType a b where
+    ensureType :: a -> Kl b
+
+instance EnsureType Val [Char] where
+    ensureType (VStr s) = return s
+    ensureType notStr   =
+      throwError $ TypeError { foundTy = typeOf notStr, expectedTy = TyStr }
+
+instance EnsureType Val Bool where
+    ensureType (VBool b) = return b
+    ensureType notBool   =
+      throwError $ TypeError { foundTy = typeOf notBool, expectedTy = TyBool }
+
+instance EnsureType Val [Val] where
+    ensureType (VList l) = return l
+    ensureType notList =
+      throwError $ TypeError { foundTy = typeOf notList, expectedTy = TyList }
+
+instance EnsureType Val Double where
+    ensureType (VNum n) = return n
+    ensureType notNum   =
+      throwError $ TypeError { foundTy = typeOf notNum, expectedTy = TyNum }
+
+instance EnsureType Val Int where
+    ensureType (VNum n) = return $ floor n
+    ensureType notNum   =
+      throwError $ TypeError { foundTy = typeOf notNum, expectedTy = TyNum }
+
+instance EnsureType Val Func where
+    ensureType (VFun f) = return f
+    ensureType notFun   =
+      throwError $ TypeError { foundTy = typeOf notFun, expectedTy = TyFun }
+
+instance EnsureType Val Symbol where
+    ensureType (VSym s) = return $ s
+    ensureType notSym =
+      throwError $ TypeError { foundTy = typeOf notSym, expectedTy = TySym }
+
 class KlVal a where
     klVal :: a -> Val
 
@@ -45,17 +84,20 @@ instance KlVal Int    where klVal = VNum . fromIntegral
 instance KlVal Bool   where klVal = VBool
 instance KlVal Char   where klVal = VStr . (:[])
 instance KlVal Double where klVal = VNum
-instance KlVal a => KlVal [a]
-    where klVal a = VList $ map klVal a
-instance KlVal ([Exp] -> Kl Val)
-    where klVal = VFun . StdFun
+instance KlVal a => KlVal [a]  where klVal a = VList $ map klVal a
 
 newtype Kl a = Kl { runKl :: StateT Env (ErrorT KlException IO) a }
     deriving ( Functor, Applicative, Monad, MonadState Env
              , MonadIO, MonadError KlException )
 
+class KlFun a where
+    apply :: a -> Exp -> Kl Val
+
+instance (KlFun f) => KlFun (Exp -> f) where
+    apply f e = return (VFun $ StdFun $ f e)
+
 data Func = Closure Env Symbol Exp
-          | StdFun ([Exp] -> Kl Val)
+          | forall f. (KlFun f) => StdFun f
 
 instance Show Func where show _ = "func"
 
@@ -78,11 +120,6 @@ typeOf VStr{}  = TyStr
 typeOf VNum{}  = TyNum
 typeOf VList{} = TyList
 typeOf VFun{}  = TyClos
-
-ensureType :: Val -> Type -> Kl Val
-ensureType val ty
-  | typeOf val == ty = return val
-  | otherwise  = throwError $ TypeError { foundTy = typeOf val, expectedTy = ty }
 
 ensureArity :: Int -> [a] -> Kl [a]
 ensureArity n l
