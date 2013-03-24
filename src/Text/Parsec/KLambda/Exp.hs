a25888f6d78828d341739b4c69da14d1a1d58d87
osa1 <omeragacan@gmail.com>
2013-03-24 09:21:53 +0200

    minor refactoring on Parsec/Lexer interface

--- src/Text/Parsec/KLambda/Exp.hs
+++ src/Text/Parsec/KLambda/Exp.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE FlexibleContexts, NoMonomorphismRestriction #-}
 {-# OPTIONS_GHC -Wall #-}
 
-module Text.Parsec.KLambda where
+module Text.Parsec.KLambda.Exp where
 
 import KLambda.Lexer
 
@@ -9,7 +9,7 @@ import Text.Parsec hiding (satisfy)
 
 type Parser = Parsec [KlToken] ()
 
-satisfy :: (Stream [KlToken] m KlToken) => (KlToken -> Bool) -> ParsecT [KlToken] u m KlTok
+satisfy :: Stream [KlToken] m KlToken => (KlTok -> Bool) -> ParsecT [KlToken] u m KlTok
 satisfy f = tokenPrim show nextPos tokeq
   where nextPos :: SourcePos -> KlToken -> [KlToken] -> SourcePos
         nextPos pos _ ((_, (Right (AlexPn _ l c))):_) = setSourceColumn (setSourceLine pos l) c
@@ -17,26 +17,26 @@ satisfy f = tokenPrim show nextPos tokeq
         nextPos pos _ []                              = pos
 
         tokeq :: KlToken -> Maybe KlTok
-        tokeq t = if f t then Just (fst t) else Nothing
+        tokeq (t, _) = if f t then Just t else Nothing
 
 tok :: (Stream [KlToken] m KlToken) => KlTok -> ParsecT [KlToken] u m KlTok
-tok t = satisfy (\(t', _) -> t' == t) <?> show t
+tok t = satisfy (\t' -> t' == t) <?> show t
 
 string, num, anySymbol :: Monad m => ParsecT [KlToken] u m KlTok
 
 string = satisfy p <?> "string"
-  where p (t, _) = case t of Str _ -> True
-                             _ -> False
+  where p t = case t of Str _ -> True
+                        _ -> False
 
 num = satisfy p <?> "number"
-  where p (t, _) = case t of Number _ -> True
-                             _ -> False
+  where p t = case t of Number _ -> True
+                        _ -> False
 
 anySymbol = satisfy p <?> "symbol"
-  where p (t, _) = case t of Symbol _ -> True
-                             _ -> False
+  where p t = case t of Symbol _ -> True
+                        _ -> False
 
 symbol :: Monad m => String -> ParsecT [KlToken] u m KlTok
 symbol s = satisfy p <?> "symbol \"" ++ s ++ "\""
-  where p (t, _) = case t of Symbol s' -> if s' == s then True else False
-                             _ -> False
+  where p t = case t of Symbol s' -> if s' == s then True else False
+                        _ -> False
