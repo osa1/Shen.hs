d1c4da9dcb5ab053c4d51b2fa948dfe9b09b9b4d
osa1 <omeragacan@gmail.com>
2013-04-06 06:47:24 +0300

    toList helper for converting IOVector a to [a]

---
+++ src/KLambda/Utils.hs
@@ -0,0 +1,14 @@
+module KLambda.Utils where
+
+import qualified Data.Vector.Mutable as MV
+
+toList :: MV.IOVector a -> IO [a]
+toList vec = iter vec 0
+  where iter :: MV.IOVector a -> Int -> IO [a]
+        iter vec n =
+          if n < MV.length vec
+            then do
+              e    <- MV.unsafeRead vec n
+              rest <- iter vec (n+1)
+              return (e:rest)
+            else return []
