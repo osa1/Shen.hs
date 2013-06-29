88a324ea3dcde75b6e1d5e4c38cac0e274a860dc
osa1 <omeragacan@gmail.com>
2013-06-29 11:13:38 +0300

    comment debug functions, remove testF

--- dynload_test/Debug.hs
+++ dynload_test/Debug.hs
@@ -7,6 +7,7 @@ import           Control.Monad.IO.Class (liftIO)
 import           Prelude                hiding (exp, read)
 import           Text.Parsec
 
+-- | Parse parameter using KLambda parser and print result. Returns unit.
 parseVal :: KlFun1
 parseVal val = do
     liftIO $ print val
@@ -15,34 +16,34 @@ parseVal val = do
       Right exp -> liftIO $ print exp
     return VUnit
 
+-- | Print first parameter and return second parameter. It's used to trace
+-- function calls.
 debugPrint :: KlFun2
 debugPrint str val = do
     str' <- ensureType str
     liftIO $ putStrLn str'
     return val
 
+-- | Print second parameter appended to first parameter(a string) and
+-- return second parameter. It's used to trace evaluated expressions.
 debug :: KlFun2
 debug str val = do
     str' <- ensureType str
     liftIO $ putStrLn (str' ++ show val ++ "\n")
     return val
 
+-- | Print first parameter, then second parameter(with it's type) and
+-- return third parameter.
 debug1 :: KlFun3
 debug1 str val retval = do
     str' <- ensureType str
     liftIO $ putStrLn $ concat [ str', show val, " :: ", show $ typeOf val, "\n" ]
     return retval
 
-test :: KlFun0
-test = do
-    liftIO $ putStrLn "test"
-    return VUnit
-
 -- Foreign interface
 
-parseValF, debugPrintF, debugF, debug1F, testF :: KlFun1
+parseValF, debugPrintF, debugF, debug1F :: KlFun1
 parseValF   = mkFun1 parseVal
 debugPrintF = mkFun1 debugPrint
 debugF      = mkFun1 debug
 debug1F     = mkFun1 debug1
-testF       = mkFun1 test
